ffe5ee6f66815662cd369424119090f122de5e51
Noman Dipto <noman40046@gmail.com>
2024-08-01 12:22:21 -0700

    work page not done

--- src/components/Pages/Work/Work.jsx
+++ src/components/Pages/Work/Work.jsx
@@ -8,9 +8,11 @@ const Work = () => {
       </h1>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-5 p-4">
         
+
+
         <div class="group cursor-pointer relative">
           <img
-            src="/public/work-img/img-5.png"
+            src="/public/work-img/img-1.png"
             alt="Image 1"
             class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
           />
@@ -20,15 +22,15 @@ const Work = () => {
               <h1 className="font-bold text-white">Portfolio</h1>
               <h2 className="text-gray-400">Website</h2>
             </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
+                  <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                  href="https://noman40046.github.io/javascript-jquary-project-6/"
+                  target="_blank">Visit Site</a>
           </div>
         </div>
 
         <div class="group cursor-pointer relative">
           <img
-            src="/public/work-img/img-2.png"
+            src="/public/work-img/img-4.png"
             alt="Image 1"
             class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
           />
@@ -38,85 +40,9 @@ const Work = () => {
               <h1 className="font-bold text-white">Login Form</h1>
               <h2 className="text-gray-400">Web Application</h2>
             </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
-          </div>
-        </div>
-
-
-        <div class="group cursor-pointer relative">
-          <img
-            src="/public/work-img/img-1.png"
-            alt="Image 1"
-            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-          />
-           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
-          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
-            <div className="grid place-items-center mb-2">
-              <h1 className="font-bold text-white">Digital Counter</h1>
-              <h2 className="text-gray-400">Web Application</h2>
-            </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
-          </div>
-        </div>
-
-
-        <div class="group cursor-pointer relative">
-          <img
-            src="/public/work-img/img-8.png"
-            alt="Image 1"
-            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-          />
-           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
-          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
-            <div className="grid place-items-center mb-2">
-              <h1 className="font-bold text-white">Resturent</h1>
-              <h2 className="text-gray-400">Website</h2>
-            </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
-          </div>
-        </div>
-
-
-        <div class="group cursor-pointer relative">
-          <img
-            src="/public/work-img/img-9.png"
-            alt="Image 1"
-            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-          />
-           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
-          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
-            <div className="grid place-items-center mb-2">
-              <h1 className="font-bold text-white">Corporate</h1>
-              <h2 className="text-gray-400">Website</h2>
-            </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
-          </div>
-        </div>
-
-
-        <div class="group cursor-pointer relative">
-          <img
-            src="/public/work-img/img-10.png"
-            alt="Image 1"
-            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-          />
-          <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
-          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
-            <div className="grid place-items-center mb-2">
-              <h1 className="font-bold text-white">Quiz</h1>
-              <h2 className="text-gray-400"> Web Application</h2>
-            </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                  href="https://noman40046.github.io/html-css-bootstrap-project-10/"
+                  target="_blank">Visit Site</a>
           </div>
         </div>
 
@@ -130,19 +56,95 @@ const Work = () => {
            <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
           <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
             <div className="grid place-items-center mb-2">
-              <h1 className="font-bold text-white">Basic Calculator</h1>
+              <h1 className="font-bold text-white">Digital Counter</h1>
               <h2 className="text-gray-400">Web Application</h2>
             </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                 href="https://noman40046.github.io/project-12/"
+                  target="_blank">Visit Site</a>
           </div>
         </div>
 
 
         <div class="group cursor-pointer relative">
           <img
-            src="/public/work-img/img-4.png"
+            src="/public/work-img/img-10.png"
+            alt="Image 1"
+            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
+          />
+           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
+          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
+            <div className="grid place-items-center mb-2">
+              <h1 className="font-bold text-white">Resturent</h1>
+              <h2 className="text-gray-400">Website</h2>
+            </div>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                href="https://noman40046.github.io/html-css-bootstrap-project-5/"
+                  target="_blank">Visit Site</a>
+          </div>
+        </div>
+
+
+        <div class="group cursor-pointer relative">
+          <img
+            src="/public/work-img/img-6.png"
+            alt="Image 1"
+            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
+          />
+           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
+          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
+            <div className="grid place-items-center mb-2">
+              <h1 className="font-bold text-white">Corporate</h1>
+              <h2 className="text-gray-400">Website</h2>
+            </div>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                   href="https://noman40046.github.io/project-14/"
+                  target="_blank">Visit Site</a>
+          </div>
+        </div>
+
+
+        <div class="group cursor-pointer relative">
+          <img
+            src="/public/work-img/img-5.png"
+            alt="Image 1"
+            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
+          />
+          <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
+          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
+            <div className="grid place-items-center mb-2">
+              <h1 className="font-bold text-white">Quiz</h1>
+              <h2 className="text-gray-400"> Web Application</h2>
+            </div>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                      href="https://noman40046.github.io/Portfoilio-website/"
+                  target="_blank">Visit Site</a>
+          </div>
+        </div>
+
+
+        <div class="group cursor-pointer relative">
+          <img
+            src="/public/work-img/img-9.png"
+            alt="Image 1"
+            class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
+          />
+           <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 rounded-lg scale-105 transition-opacity z-10"></div>
+          <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
+            <div className="grid place-items-center mb-2">
+              <h1 className="font-bold text-white">Basic Calculator</h1>
+              <h2 className="text-gray-400">Web Application</h2>
+            </div>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                    href="https://noman40046.github.io/html-css-b-jquay-project-4/"
+                  target="_blank">Visit Site</a>
+          </div>
+        </div>
+
+
+        <div class="group cursor-pointer relative">
+          <img
+            src="/public/work-img/img-8.png"
             alt="Image 1"
             class="w-full object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
           />
@@ -152,9 +154,9 @@ const Work = () => {
               <h1 className="font-bold text-white">Chatbot</h1>
               <h2 className="text-gray-400">Web Application</h2>
             </div>
-            <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-              View
-            </button>
+            <a class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
+                    href="https://noman40046.github.io/html-css-b-jquay-project-4/"
+                  target="_blank">Visit Site</a>
           </div>
         </div>
 
